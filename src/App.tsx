1299edd53fa24bf3d24c4129b858bff64fbdb942
pavvelpotapov <pavelminsk1979@mail.ru>
2025-02-21 12:19:50 +0300

    connection with back

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import axios from "axios";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [value, setValue] = useState('nothing')
+
+    const handleOnClick = () => {
+        axios.post('http://localhost:3010', {value})
+            .then(res => {
+                console.log(res.data)
+                setValue(res.data);
+            })
+            .catch(error => {
+                console.log(error.message)
+                setValue(error.message);
+            })
+    }
+
+    return (
+        <div className="App">
+            <div> HELLOY!</div>
+
+            <button
+                onClick={handleOnClick}
+            > ЗАПРОС НА БЭК
+            </button>
+            <div>{value}</div>
+        </div>
+
+    );
 }
 
 export default App;
+
+
+
