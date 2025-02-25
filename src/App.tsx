162c94cba75a1cbdefe81a389d35a27f36339763
pavvelpotapov <pavelminsk1979@mail.ru>
2025-02-25 19:01:38 +0300

    Send image for yandex

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,14 @@ import './App.css';
 function App() {
 
 
+
     return (
+        <>
 
             <form
                 action="http://localhost:3010/image"
-                  encType="multipart/form-data"
-                  method="POST">
+                encType="multipart/form-data"
+                method="POST">
                 <input
                     type="text"
                     name="Имя и Фамилия"/>
@@ -18,52 +20,39 @@ function App() {
                     name="imageimage"
                     required/>
                 <button>ЗАПРОС НА БЭК</button>
+
+            </form>
+
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+
+            <form
+                action="http://localhost:3010/yandex"
+                encType="multipart/form-data"
+                method="POST">
+                <input
+                    type="text"
+                    name="Имя и Фамилия"/>
+                <input
+                    type="file"
+                    name="yandex"
+                    required/>
+                <button>ПОМЕСТИТЬ В ЯНДЕКС ОБЛАКО</button>
+
             </form>
 
 
+        </>
 
-    );
+
+    )
+
 }
 
 export default App;
 
 
-/*
-import React, {useState} from 'react';
-import './App.css';
-import axios from "axios";
 
-function App() {
-    const [value, setValue] = useState('nothing')
-
-    const handleOnClick = () => {
-        axios.post('http://localhost:3010', {value})
-            .then(res => {
-                console.log(res.data)
-                setValue(res.data);
-            })
-            .catch(error => {
-                console.log(error.message)
-                setValue(error.message);
-            })
-    }
-
-    return (
-        <div className="App">
-            <div> HELLOY!</div>
-
-            <button
-                onClick={handleOnClick}
-            > ЗАПРОС НА БЭК
-            </button>
-            <div>{value}</div>
-        </div>
-
-    );
-}
-
-export default App;
-
-
-*/
 
