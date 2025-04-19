8b964bbbfd54d4b957d44787de69504de0436e5e
pavvelpotapov <pavelminsk1979@mail.ru>
2025-04-19 03:22:11 +0300

    start for work with files and strims/path4

--- src/options/OptionSimple.tsx
+++ src/options/OptionSimple.tsx
@@ -1,25 +1,32 @@
-import React, { useState } from "react";
+import React, {useState} from "react";
 import axios from "axios";
 
 export const OptionSimple = () => {
     const [file, setFile] = useState<File | null>(null);
 
     const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        console.log('ФАЙЛ ПОПАДАЕТ В USESTATE')
         setFile(event.target.files?.[0] || null);
     };
 
     const handleOnClick = async () => {
+        console.log('ИДЕТ ОТПРАВКА НА БЭК ФАЙЛА. ФАЙЛ ПРИСУТСТВУЕТ?', file)
         if (!file) {
             console.log('Файл не выбран!');
             return;
         }
 
         const formData = new FormData();
-        formData.append('file', file);
+        // 'streamstream'  это как ключь :
+        //  file   это значение, это сама картинка
+        // так можно заполнять передаваемый на бэк обьект  formData
+        formData.append('streamstream', file);
+        formData.append('deskription', 'text,text,text');
 
         try {
-            await axios.post('http://localhost:3010/stream', formData);
+            const res = await axios.post('http://localhost:3010/stream', formData);
             console.log('Файл успешно загружен!');
+            console.log('УРЛ АДРЕС КАРТИНКИ', res)
         } catch (e) {
             console.log('Ошибка загрузки файла', e);
         }
@@ -27,7 +34,7 @@ export const OptionSimple = () => {
 
     return (
         <>
-            <input type="file" onChange={handleFileChange} />
+            <input type="file" onChange={handleFileChange}/>
             <button onClick={handleOnClick}>Загрузить файл</button>
         </>
     );
